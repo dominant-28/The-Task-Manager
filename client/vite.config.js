ec900949f1886c4ce7ffc3f2ad6ccf4027fa6f0f
SOHAM KALE <157865836+dominant-28@users.noreply.github.com>
2025-07-22 22:24:16 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target:'https://the-task-manager-rkxd.vercel.app',
+        target:'https://the-task-manager.onrender.com',
         changeOrigin:true,
       }
     }
