e72db9c279480fc2de6ed284d7e8d3a33211a373
SOHAM KALE <157865836+dominant-28@users.noreply.github.com>
2025-07-23 01:18:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-import MongoStore from "connect-mongo";
+
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import dotenv from "dotenv"
 import express from "express"
 import morgan from "morgan"
-import session from "express-session";
-import passport from "./config/passport.js";
+
+
 import dbConnection from "./utils/index.js"
 import { errorhandler,routeNotFound } from "./middlewares/errorMiddleware.js";
 import routes from "./routes/index.js";
@@ -33,25 +33,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(morgan("dev"))
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false, // better: don’t save empty sessions
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URL,  // <-- your DB
-      collectionName: "sessions"
-    }),
-    cookie: {
-      secure: process.env.NODE_ENV !== "development",
-      httpOnly: true,
-      sameSite: "None",
-      maxAge: 24 * 60 * 60 * 1000
-    }
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 app.use("/api",routes)
  app.use(routeNotFound)
  app.use(errorhandler)
