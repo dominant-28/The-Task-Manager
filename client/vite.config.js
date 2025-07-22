58a2e0d98afc09ad3883d9bb15ac9882437d766c
SOHAM KALE <157865836+dominant-28@users.noreply.github.com>
2025-07-22 22:23:13 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{
-//     port:3000,
-//     proxy:{
-//       '/api':{
-//         target:'https://the-task-manager-rkxd.vercel.app',
-//         changeOrigin:true,
-//       }
-//     }
-//   }
-// })
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    port:3000,
+    proxy:{
+      '/api':{
+        target:'https://the-task-manager-rkxd.vercel.app',
+        changeOrigin:true,
+      }
+    }
+  }
+})
