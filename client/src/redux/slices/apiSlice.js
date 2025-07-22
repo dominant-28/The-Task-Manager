d249777685c7a44d3e6f3ba03a733f8574c60ee6
SOHAM KALE <157865836+dominant-28@users.noreply.github.com>
2025-07-23 00:54:06 +0530

    Update apiSlice.js

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -3,7 +3,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 // const API_URI="http://localhost:5000/api"
 const API_URI=import.meta.env.VITE_APP_BASE_URL
 const baseQuery = fetchBaseQuery({ 
-    baseUrl:API_URI+"/api"
+    baseUrl:API_URI+"/api",
      credentials: "include"
  })
 
